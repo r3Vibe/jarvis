ac10fadf11ac99d8b21c8a4b594166fbdbb7865a
r3Vibe <arnab95gupta@gmail.com>
2022-03-17 23:19:15 +0530

    update 1

---
+++ jarvis.py
@@ -0,0 +1,28 @@
+from Module import Recognizer,ExtractCommand,ExecuteCommand,Speak
+import os
+from time import sleep
+
+
+def listen_for_keyword():
+    os.system("cls")
+    while True:
+        keyw = Recognizer().listen()
+        if "jarvis" in keyw:
+            Speak().speakout("Yes?")
+            listen_for_command()
+        else:
+            continue
+
+
+def listen_for_command():
+    print("Listening....")
+    command = Recognizer().listen()
+    Speak().speakout("Ok")
+    get_cmd = ExtractCommand(command).Extract()
+    ExecuteCommand(get_cmd).Execute()
+    sleep(2)
+    listen_for_keyword()
+
+
+if __name__ == "__main__":
+    listen_for_keyword()
